6b4c960f2b527e953377488bd39d2dfb99f68711
RachitShrivastava <rachitshrivastava15@gmail.com>
2022-09-13 12:11:02 -0400

    TwoSum_LeetCode_C#

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums ={2,7,11,15};
+            int target = 9;
+
+            int[] result = TwoSum(nums, target);
+            Console.WriteLine(string.Join(" ", result));
+            
+        }
+
+        private static int[] TwoSum(int[] nums, int target)
+        {
+            int length = nums.Length;
+            Dictionary<int, int> dict = new();
+
+            for(int i = 0; i < length;i++)
+            {
+                int firstNum =  nums[i];
+                int diff = target - firstNum;
+                if(dict.TryGetValue(diff, out int index))
+                {
+                    return new int[] {index, i};
+                    
+                }
+                dict[firstNum] = i;
+            }
+            return Array.Empty<int>();
+        }
+    }
+}
